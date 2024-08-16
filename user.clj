cb2153279931f3537140d8a84335a73fbf989147
David Liman <limanoit@gmail.com>
2024-08-15 17:57:20 -0700

    tags tell same/diff values

--- user.clj
+++ user.clj
@@ -8,9 +8,32 @@
   (swap! log-store update-in [tag] #(conj (or % []) val))
   val)
 
+(defn logs
+  [tag & functions]
+  (let [tag (if (number? tag)
+              (nth (keys @log-store) tag)
+              tag)]
+    (loop [values    (@log-store tag)
+           functions functions]
+      (if (seq functions)
+        (recur ((first functions) values)
+               (rest functions))
+        values))))
+
+#_(defn tags []
+  (->> @log-store
+       (reduce-kv #(assoc %1 [(count %3) %2] (last %3)) {})
+       (map-indexed hash-map)
+       (into [])))
+
+(defn same-values? [x]
+  (if (= 1 (count (set x)))
+    :same-values
+    :different-values))
+
 (defn tags []
   (->> @log-store
-       (reduce-kv #(assoc %1 [(count %3) %2] (first %3)) {})
+       (reduce-kv #(assoc %1 [(count %3) (same-values? %3) %2] (last %3)) {})
        (map-indexed hash-map)
        (into [])))
 
@@ -20,8 +43,17 @@
 (defn lazy-fn [symbol]
   (fn [& args] (apply (requiring-resolve symbol) args)))
 
-(def go (lazy-fn 'integrant.repl/go))
-(def halt (lazy-fn 'integrant.repl/halt))
-(def clear (lazy-fn 'integrant.repl/clear))
-(def reset (lazy-fn 'integrant.repl/reset))
-(def reset-all (lazy-fn 'integrant.repl/reset-all))
+(defn app []
+  ((lazy-fn 'shadow.cljs.devtools.api/watch) :app))
+
+(defn x [x y] (* 15 (- x y)))
+(defn t [x y] (* 20 (- x y)))
+(defn f [x y] (* 40 (- x y)))
+
+;; june 18
+(+
+ (* 10 (- 3.27 4.96))
+ 3
+ )
+;; => -13.899999999999999
+;; => -16.9
